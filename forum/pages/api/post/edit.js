2c75e44fce41fec1006b2ec73b9b2eb3561e0b24
ARKNOOB1 <a59114154@gmail.com>
2024-07-30 15:40:54 +0900

    7_30

---
+++ forum/pages/api/post/edit.js
@@ -0,0 +1,15 @@
+import {connectDB} from '@/utils/database.js'
+import { ObjectId } from "mongodb";
+
+export default async function handler(요청, 응답) {
+    if (요청.method == 'POST') {
+        let Cdata = {title : 요청.body.title, content : 요청.body.content};
+        let db = (await connectDB).db('forum');
+        let result = await db.collection('post').updateOne(
+            {_id : new ObjectId(요청.body._id)},
+            {$set : Cdata}
+        );
+        console.log(result)
+        응답.redirect(302,'/list');
+    }
+}
